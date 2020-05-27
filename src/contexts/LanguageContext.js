8328beb2d5e8d82c4012f39c8ce941b5f4924eca
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-05-27 17:20:11 +0300

    Add files via upload

---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+
+const Context = React.createContext('English')
+
+export class LanguageStore extends Component {
+    state = {language: 'English'}
+
+    onLanguageChange = (language) => {
+        this.setState({language})
+    }
+
+    render() {
+        return (
+            <Context.Provider value={{...this.state, onLanguageChange: this.onLanguageChange}}>
+                {this.props.children}
+            </Context.Provider>
+        )
+    }
+}
+
+export default Context
